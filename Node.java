90d9db7c58d6a9a06ee64e195873169b2ef9edc6
i has candys <ichen10@stuy.edu>
2021-01-08 01:58:42 -0500

    made get/set methods in Node class

--- Node.java
+++ Node.java
@@ -1,6 +1,27 @@
-public class Node{
- public Node(String value){/*create a constructor*/}
- private String data;
- private Node next,prev;
- //write get/set methods for all three instance variables.
+public class Node {
+	public Node(String value) {
+		data = value;
+	}
+	private String data;
+	private Node next,prev;
+	//write get/set methods for all three instance variables.
+	public String data() {
+		return data;
+	}
+	public Node next() {
+		return next;
+	}
+	public Node prev() {
+		return prev;
+	}
+	public String setData(String data) {
+		this.data = data;
+	}
+	public Node setNext(Node node) {
+		next = node;
+	}
+	public Node setPrev(Node node) {
+		prev = node;
+	} 
+
 }
